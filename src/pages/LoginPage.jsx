e766efaeace94365d71af58fdaf2690064b9be34
Eduardo <eduardo.nunes.nonato@gmail.com>
2025-08-17 19:37:41 -0300

    feat: LoginPage - Inclusão CSS

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -35,16 +35,24 @@ function LoginPage() {
     }
 
     function setRememberLogin(token) {
-        console.log(`Guarda o token do usuário por um tempo. IsRemember: ${isRememberMeActive}`)
+        console.log(`Guarda o token do usuário por um tempo. IsRemember: ${token}`)
     }
 
     return (
         <div className="loginContainer">
-            <div id="part1">
-                <img src="/../assets/images/login.png" alt="Imagem background tela de login"/>
-            </div>
-            <div id="part2">
-                <h1>Login</h1>
+            <section id="leftSection">
+                <header>
+                  <h1>Bem-vindo</h1>
+                  <p>Encontre seu melhor amigo aqui</p>
+                </header>
+                <div id="imgContainer">
+                  <img src="src/assets/images/login.png" alt="Imagem background tela de login"/>
+                </div>
+            </section>
+            <section id="rightSection">
+                <header>
+                  <h1>Login</h1>
+                </header>
                 <form onSubmit={handleSubmit}>
                     <input
                         type="email"
@@ -60,34 +68,44 @@ function LoginPage() {
                         onChange={(e) => setPassword(e.target.value)}
                         required
                     /><br/>
-                    <label>
-                        <input
-                            type="checkbox"
-                            checked={isRememberMeActive}
-                            onChange={(e) => setRemember(e.target.checked)}
-                        />
-                        Lembre de mim
-                    </label>
-                    <button
-                        type="button"
-                        onClick={handleNavigateToForgotPassword}>
-                        Esqueci minha senha
-                    </button><br/>
+                    <div className="divBoxHelper">
+                      <label>
+                          <input
+                              type="checkbox"
+                              checked={isRememberMeActive}
+                              className="checkboxClass"
+                              onChange={(e) => setRemember(e.target.checked)}
+                          />
+                          Lembre de mim
+                      </label>
+                      <button
+                          type="button"
+                          className="withoutDecorationClass"
+                          onClick={handleNavigateToForgotPassword}>
+                          Esqueceu a senha?
+                      </button>
+                    </div>
                         {error && <p style={{ color: "red" }}>{error}</p>}
-                    <button 
-                        type="submit">
-                        Entrar
-                    </button><br/>
-                    <label>Não tem uma conta?</label> 
-                    <button 
-                        type="button"
-                        onClick={handleNavigateToRegister}>
-                        Cadastre-se
-                    </button>
+                    <div className="divBoxHelper02">
+                      <button 
+                          id="loginButton"
+                          type="submit">
+                          Entrar
+                      </button><br/>
+                    </div>
+                    <div className="divBoxHelper">
+                      <label>Não tem uma conta?</label> 
+                      <button 
+                          type="button"
+                          className="withoutDecorationClass"
+                          onClick={handleNavigateToRegister}>
+                          Cadastre-se
+                      </button>
+                    </div>
                 </form>
-            </div>
+            </section>
         </div>
     )
 }
 
-export default LoginPage
+export default LoginPage  
