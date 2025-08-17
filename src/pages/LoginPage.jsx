466532e6d66e28edfa2c1533c75881fe35c32f88
Eduardo <eduardo.nunes.nonato@gmail.com>
2025-08-16 22:34:57 -0300

    feat: LoginPage - Inclusão de botões e funções

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,52 +1,93 @@
 import { useState } from "react"
+import "./LoginPage.css"
 
 function LoginPage() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState("")
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [isRememberMeActive, setRemember] = useState(false)
+    const [error, setError] = useState("")
 
-    if (!email.includes("@")) {
-      setError("E-mail inválido. Favor digitar um e-mail válido")
-      return
+    const handleSubmit = (e) => {
+        e.preventDefault()    
+        if (!email.includes("@")) {
+            setError("E-mail inválido. Favor digitar um e-mail válido")
+            return
+        } 
+        if (password.length < 8) {
+            setError("A senha deve ter pelo menos 8 caracteres")
+            return
+        } 
+        console.log(isRememberMeActive)
+        if (isRememberMeActive) {
+            console.log(isRememberMeActive)
+            setRememberLogin("TOKEN")
+        } 
+        setError("")
     }
 
-    if (password.length < 8) {
-      setError("A senha deve ter pelo menos 8 caracteres")
-      return
+    function handleNavigateToForgotPassword() {
+        console.log("Chama ForgotPasswordPage")
     }
 
-    console.log(`Email: ${email},  Password: ${password}`)
-    setError("")
-  }
+    function handleNavigateToRegister() {
+        console.log("Chama RegisterPage")
+    }
 
-  return (
-    <div className="loginContainer">
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="E-mail"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Senha"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        {error && <p style={{ color: "red" }}>{error}</p>}
-        <button type="submit">Entrar</button>
-      </form>
+    function setRememberLogin(token) {
+        console.log(`Guarda o token do usuário por um tempo. IsRemember: ${isRememberMeActive}`)
+    }
 
-      <button type="button">Esqueci minha senha</button>
-    </div>
-  )
+    return (
+        <div className="loginContainer">
+            <div id="part1">
+                <img src="/../assets/images/login.png" alt="Imagem background tela de login"/>
+            </div>
+            <div id="part2">
+                <h1>Login</h1>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="email"
+                        placeholder="E-mail"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    /><br/>
+                    <input
+                        type="password"
+                        placeholder="Senha"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    /><br/>
+                    <label>
+                        <input
+                            type="checkbox"
+                            checked={isRememberMeActive}
+                            onChange={(e) => setRemember(e.target.checked)}
+                        />
+                        Lembre de mim
+                    </label>
+                    <button
+                        type="button"
+                        onClick={handleNavigateToForgotPassword}>
+                        Esqueci minha senha
+                    </button><br/>
+                        {error && <p style={{ color: "red" }}>{error}</p>}
+                    <button 
+                        type="submit">
+                        Entrar
+                    </button><br/>
+                    <label>Não tem uma conta?</label> 
+                    <button 
+                        type="button"
+                        onClick={handleNavigateToRegister}>
+                        Cadastre-se
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default LoginPage
